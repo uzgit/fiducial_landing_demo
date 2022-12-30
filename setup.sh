b0e64c16c0bc8967e8e3a2c47e484b67bbd78d4e
Joshua Springer <jspri17@gmail.com>
2022-12-30 16:02:33 +0000

    Add files via upload
    Changed some setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ echo "installing..."
 alias_ll="alias ll=\"ls -lah\""
 grep -E "$alias_ll" ~/.bashrc || echo "$alias_ll" >> ~/.bashrc
 
-sudo apt-get install vim htop tree mlocate git
+sudo apt-get -y install vim htop tree mlocate git xdotool python3-opencv
 
-pip3 install numpy scipy opencv-python
+pip3 install numpy
+
+git clone --recursive https://github.com/pupil-labs/apriltags.git
+cd apriltags
+pip3 install -e .[testing]
